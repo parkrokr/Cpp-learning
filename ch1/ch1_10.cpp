6d2085dced6756fb2be77b73d6c796c8bf43500c
parkrokr <parkrokr@gmail.com>
2020-11-02 02:48:57 +0900

    study code

---
+++ ch1/ch1_10.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b); // forward declaration 전방선언 
+int multiply(int a, int b);
+int subtract(int a, int b);
+//Peek Definition, GO To Definition, Go To Declaration 
+
+int main()
+{
+	cout << add(1, 2) << endl;
+	cout << subtract(1, 2) << endl;
+
+	return 0;
+}
+
+
+//difinition
+int add(int a, int b) // prototype
+{
+	return a * b;
+}
+
+
+int multiply(int a, int b)
+{
+	return a * b;
+}
+
+
+int subtract(int a, int b)
+{
+	return a - b;
+}
+
+
