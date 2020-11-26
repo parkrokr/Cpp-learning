82aa02e1224caccf71f41f193e65705068fb88b1
parkrokr <parkrokr@gmail.com>
2020-11-26 19:02:12 +0900

    study code

--- ch4/ch4_2/MyConstants4_2.cpp
+++ ch4/ch4_2/MyConstants4_2.cpp
@@ -1,7 +1,6 @@
-
-
 namespace Constants
 {
+	// 다른 cpp파일에서도 사용해야 하기 떄문에 extern
 	extern const double pi(3.141592);
 	extern const double gravity(9.8);
 	// . . .
--- ch4/ch4_2/MyConstants4_2.h
+++ ch4/ch4_2/MyConstants4_2.h
@@ -2,7 +2,9 @@
 
 namespace Constants
 {
-	const double pi(3.141592);
-	const double gravity(9.8);
+	// 앞에 extern 붙이고 선언만 해주기
+	// 선언과 body를 구분하지 않으면 헤더파일을 include할때마다 사본이 생김(다른 주소를 사용)
+	extern const double pi;
+	extern const double gravity;
 	// . . .
 }
